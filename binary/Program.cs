2bcf82867fcbed0e5404206a7ec71ad8393c791f
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-08-17 21:01:19 -0600

    create binary patter 0 1 0 1 1 0 1 1 1 ...

--- binary/Program.cs
+++ binary/Program.cs
@@ -1,1 +1,31 @@
+﻿using System;
 
+namespace binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            int k = 2;
+            int nums = 30;
+            int[] numsArr = new int[30];
+
+            for (int i = 0; i < nums; i++)
+            {
+                if (i == j)
+                {
+                    numsArr[i] = 0;
+                    j = j + k;
+                    k++;
+                } else
+                {
+                    numsArr[i] = 1;
+                }
+            }
+            foreach (var num in numsArr) {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
